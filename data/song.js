e25a1132fe3d19e1521e4d14c36f0a59e79b47f2
duonghiep416 <duonghiep416@gmail.com>
2023-08-20 23:13:40 +0700

    HTML CSS Homepage

---
+++ data/song.js
@@ -0,0 +1,29 @@
+const song = [
+    {
+        name: "Trước khi em tồn tại",
+        singer: "Việt Anh",
+        thumbnail: "https://i3.ytimg.com/vi/jtFsZRHUyPE/maxresdefault.jpg",
+        album: "Sad Music",
+        playlist: null,
+        genre: "Modern Music",
+        path: "./assets/music/listSong5/truoc-khi-em-ton-tai.mp3",
+    },
+    {
+        name: "id 072019 | 3107",
+        singer: "W/n",
+        thumbnail: "https://i3.ytimg.com/vi/leJb3VhQCrg/maxresdefault.jpg",
+        album: "Sad Music",
+        playlist: null,
+        genre: "Modern Music",
+        path: "./assets/music/listSong5/Wn-id-07201-3107-ft-267.mp3",
+    },
+    {
+        name: "QUERRY",
+        singer: "QNT",
+        thumbnail: "https://i3.ytimg.com/vi/7PxxZWrlRTc/maxresdefault.jpg",
+        album: "Sad Music",
+        playlist: null,
+        genre: "Modern Music",
+        path: "./assets/music/listSong5/QUERRY-QNTxTRUNG-TRẦN-RPT-MCK-Prod-By-RASTZ-OFFICIAL-MV_320kbps.mp3",
+    },
+];
